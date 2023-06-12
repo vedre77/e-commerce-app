e06982621a3d2676b62434683037a37879ee8263
vedre77 <vedranvitas@net.hr>
2023-06-12 08:10:19 +0200

    Simple test added

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -5,12 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SareetaApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	}
+	@Test
+	public void test() {
+		assertEquals("A", "");
 	}
 
 }
