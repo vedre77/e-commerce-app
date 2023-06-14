510860270606235948818c5fbe12361f04551bc1
vedre77 <vedranvitas@net.hr>
2023-06-14 13:00:13 +0200

    Added Order and User CRUD tests

--- starter_code/src/test/java/com/example/demo/persistence/OrderRepoTest.java
+++ starter_code/src/test/java/com/example/demo/persistence/OrderRepoTest.java
@@ -1,4 +1,71 @@
 package com.example.demo.persistence;
 
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.math.BigDecimal;
+import java.util.Optional;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class OrderRepoTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    public void testOrderCRUD() {
+        // Create a new Order
+        UserOrder order = new UserOrder();
+        // Set order properties
+        order.setTotal(BigDecimal.valueOf(100));
+        // Create a new User
+        User user = new User();
+        // Set user properties
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        entityManager.persist(user);
+        entityManager.flush();
+        order.setUser(user);
+        entityManager.persist(order);
+        entityManager.flush();
+
+        // Find the order by ID
+        Optional<UserOrder> foundOrder = orderRepository.findById(order.getId());
+
+        // Check if the order was found
+        assertTrue(foundOrder.isPresent());
+        assertEquals(order.getId(), foundOrder.get().getId());
+
+        // Update the order
+        order.setTotal(BigDecimal.valueOf(200));
+        entityManager.merge(order);
+        entityManager.flush();
+
+        // Find the updated order by ID
+        Optional<UserOrder> updatedOrder = orderRepository.findById(order.getId());
+
+        // Check if the updated order was found
+        assertTrue(updatedOrder.isPresent());
+        assertEquals(BigDecimal.valueOf(200), updatedOrder.get().getTotal());
+
+        // Delete the order
+        orderRepository.delete(order);
+
+        // Try to find the deleted order by ID
+        Optional<UserOrder> deletedOrder = orderRepository.findById(order.getId());
+
+        // Check if the deleted order was not found
+        assertFalse(deletedOrder.isPresent());
+    }
+
 }
--- starter_code/src/test/java/com/example/demo/persistence/UserRepoTest.java
+++ starter_code/src/test/java/com/example/demo/persistence/UserRepoTest.java
@@ -1,4 +1,63 @@
 package com.example.demo.persistence;
 
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class UserRepoTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testUserCRUD() {
+        // Create a new User
+        User user = new User();
+        // Set user properties
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // Find the user by ID
+        Optional<User> foundUser = userRepository.findById(user.getId());
+
+        // Check if the user was found
+        assertTrue(foundUser.isPresent());
+        assertEquals(user.getId(), foundUser.get().getId());
+
+        // Update the user
+        user.setPassword("newpassword");
+        entityManager.merge(user);
+        entityManager.flush();
+
+        // Find the updated user by ID
+        Optional<User> updatedUser = userRepository.findById(user.getId());
+
+        // Check if the updated user was found
+        assertTrue(updatedUser.isPresent());
+        assertEquals("newpassword", updatedUser.get().getPassword());
+
+        // Delete the user
+        userRepository.delete(user);
+
+        // Try to find the deleted user by ID
+        Optional<User> deletedUser = userRepository.findById(user.getId());
+
+        // Check if the deleted user was not found
+        assertFalse(deletedUser.isPresent());
+    }
 }
