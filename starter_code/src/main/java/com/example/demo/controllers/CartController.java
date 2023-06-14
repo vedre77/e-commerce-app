181e1f86b1476bd503f7ad5249afb2e54d76e07b
vedre77 <vedranvitas@net.hr>
2023-06-14 14:40:24 +0200

    Imports edit

--- starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -2,7 +2,6 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.User;
