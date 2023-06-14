6e557ecbe2393f37b0449f94f0ac4a72bfbfc2a4
vedre77 <vedranvitas@net.hr>
2023-06-14 13:50:28 +0200

    Moved creation methods to TestUtils, added Cart test methods

--- starter_code/src/test/java/com/example/demo/TestUtils.java
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -1,6 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 public class TestUtils {
 
@@ -23,5 +29,33 @@ public class TestUtils {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    public static User createUser(TestEntityManager entityManager) {
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        entityManager.persist(user);
+        entityManager.flush();
+        return user;
+    }
+
+    public static Cart createCart(User user, TestEntityManager entityManager) {
+        Cart cart = new Cart();
+        cart.setUser(user);
+        cart.setTotal(BigDecimal.ZERO);
+        entityManager.persist(cart);
+        entityManager.flush();
+        return cart;
+    }
+
+    public static Item createItem(String name, String description, BigDecimal price, TestEntityManager entityManager) {
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setPrice(price);
+        entityManager.persist(item);
+        entityManager.flush();
+        return item;
     }
 }
